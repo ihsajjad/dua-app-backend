b8fc17afbf608b247e5dbddfc875e36fdb922826
IH Sajjad <iftekherhossensajjad@gmail.com>
2025-06-26 01:31:53 +0600

    made some changes in db.ts

--- src/db.ts
+++ src/db.ts
@@ -1,8 +1,9 @@
-import Database from 'better-sqlite3';
-import path from 'path';
+import Database from "better-sqlite3";
+import path from "path";
 
 const dbPath = path.join(__dirname, "database", "dua_main.sqlite");
-console.log("DB Path: ", dbPath);
 const db = new Database(dbPath);
+const categories = db.prepare("SELECT * FROM category").all();
+console.log("DB Path: ", dbPath, categories);
 
 export default db;
