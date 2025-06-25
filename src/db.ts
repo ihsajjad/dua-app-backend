e385ca0a2de2b1a4f7f75eeeeddf54e13c483654
IH Sajjad <iftekherhossensajjad@gmail.com>
2025-06-25 16:09:07 +0600

    Fixed some mistakes in package.json to deploy the project

--- src/db.ts
+++ src/db.ts
@@ -2,6 +2,7 @@ import Database from 'better-sqlite3';
 import path from 'path';
 
 const dbPath = path.join(__dirname, "database", "dua_main.sqlite");
+console.log("DB Path: ", dbPath);
 const db = new Database(dbPath);
 
 export default db;
