dafaf554211992a3739d61ff80f50f7b9d7aa19d
IH Sajjad <iftekherhossensajjad@gmail.com>
2025-06-26 01:57:44 +0600

    Final commit

--- src/db.ts
+++ src/db.ts
@@ -3,7 +3,5 @@ import path from "path";
 
 const dbPath = path.join(__dirname, "database", "dua_main.sqlite");
 const db = new Database(dbPath);
-const categories = db.prepare("SELECT * FROM category").all();
-console.log("DB Path: ", dbPath, categories);
 
 export default db;
